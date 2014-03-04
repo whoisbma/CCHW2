50c60821c37cf528319ce059759e09c0b4da26e6
Bryan Ma <bryanhma@gmail.com>
2014-03-03 19:51:46 -0500

    Initial Commit

---
+++ SpriteKitPhysicsTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpriteKitPhysicsTest
+//
+//  Created by Bryan Ma on 3/3/14.
+//  Copyright (c) 2014 Bryan Ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
